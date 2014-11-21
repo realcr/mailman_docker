f9258b810fce664dbf9cc1c40356b139d5587973
real <real.flayer@outlook.com>
2014-11-21 09:57:05 -0500

    Back to using export. It didn't work without it.

--- conf.sh
+++ conf.sh
@@ -2,13 +2,13 @@
 # Mailman docker image.
 
 # The mail address of the owner of mailman list:
-MAILMAN_LIST_OWNER_MAIL=real.flayer@outlook.com
+export MAILMAN_LIST_OWNER_MAIL=real.flayer@outlook.com
 
 # Mailman's list owner password:
-MAILMAN_LIST_OWNER_PASS=123456
+export MAILMAN_LIST_OWNER_PASS=123456
 
 # Domain address for mailman:
-MAILMAN_DOMAIN=lists.freedomlayer.org
+export MAILMAN_DOMAIN=lists.freedomlayer.org
 
 # Site password (The highest password for Web authentication):
-MAILMAN_SITE_PASS=12345678
+export MAILMAN_SITE_PASS=12345678
