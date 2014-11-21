df2fb1ed1b1cfb346aff29efcf93ea4b8828da43
real <real.flayer@outlook.com>
2014-11-21 14:36:58 +0200

    Fixed a mistake in file destination.

--- assets/apply_conf.sh
+++ assets/apply_conf.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 # We want to replace some place holders with the right values.
 # (We use the configuration files as templates of configuration files).
 # See conf.sh for detailed information.
 
-# Get environment variables:
+# Get environment variables from conf.sh:
 source conf.sh
 
 # Replace environment variables with their value in some configuration files:
 envsubst < "etc-mailman-mm_cfg.py" > "etc-mailman-mm_cfg.py"
 envsubst < "etc-postfix-transport" > "etc-postfix-transport"
-envsubst < "etc-apache2-sites-mailman-conf" > "etc-postfix-transport"
+envsubst < "etc-apache2-sites-mailman-conf" > "etc-apache2-sites-mailman-conf"
