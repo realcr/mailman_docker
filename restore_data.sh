b47fe9d99091d52c28206f27d820dafc2a513d57
real <real.flayer@outlook.com>
2014-11-22 13:15:02 -0500

    Restore data script works now.

--- restore_data.sh
+++ restore_data.sh
@@ -3,10 +3,15 @@
 # Restore backuped date into mailman.
 # Takes the backup file name (A tar file) as argument.
 
+# Abort if file doesn't exist:
+set -e
+
 BACK_DIR="backup_temp"
 
 # Extract the Tar file into backup_temp:
 tar -xvf $1 -C ./
+
+set +e
 
 # Backup the data, lists and archives mailman directories
 # by copying them to backup_temp directory on the host:
@@ -15,9 +20,12 @@ docker run --name mailman_data_restore_cont \
 	-v $(readlink -f $BACK_DIR):/backup \
         mailman_data \
 	sh -c "\
-        cp -R /var/lib/mailman/data /backup/data && \
-        cp -R /var/lib/mailman/lists /backup/lists && \
-        cp -R /var/lib/mailman/archives /backup/archives"
+	rm -rfv /var/lib/mailman/data/* && \
+	rm -rfv /var/lib/mailman/lists/* && \
+	rm -rfv /var/lib/mailman/archives/* && \
+        cp -R /backup/data /var/lib/mailman && \
+        cp -R /backup/lists /var/lib/mailman && \
+        cp -R /backup/archives /var/lib/mailman "
 
 # Clean up: remove mailman_data_restore container:
 docker rm -f mailman_data_restore_cont
