f7b3ef9176a86e6e3554f85339c9902cfa26249a
real <real.flayer@outlook.com>
2014-11-22 12:07:24 -0500

    Added a script to start a mailman server container.

---
+++ start_server.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Start the mailman server, and connect it with the volumes 
+# inside the data container.
+
+# Get the directories contents by running a new mailman_server:
+docker run -d --name  mailman_server_cont \
+        -p 80:80 -p 25:25 \
+	--volumes-from mailman_data_cont \
+        mailman_server
