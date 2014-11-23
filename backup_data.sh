dd4407bbd4d068aec8e54f0310ad6de63dae6171
real <real.flayer@outlook.com>
2014-11-23 05:41:52 -0500

    Added p to keep permissions.

--- backup_data.sh
+++ backup_data.sh
@@ -2,22 +2,27 @@
 
 # Backup data from the mailman data container.
 
+# Set abort on error:
+set -e
+
 BACK_DIR="backup_temp"
 
-mkdir -p ./${BACK_DIR}/data
-mkdir -p ./${BACK_DIR}/lists
-mkdir -p ./${BACK_DIR}/archives
+mkdir -p ./${BACK_DIR}
+# mkdir -p ./${BACK_DIR}/data
+# mkdir -p ./${BACK_DIR}/lists
+# mkdir -p ./${BACK_DIR}/archives
 
 # Backup the data, lists and archives mailman directories
 # by copying them to backup_temp directory on the host:
+# Note: The p flag for cp preserves ownership.
 docker run --name mailman_data_backup_cont \
 	--volumes-from mailman_data_cont \
 	-v $(readlink -f $BACK_DIR):/backup \
         mailman_data \
 	sh -c "\
-        cp -R /var/lib/mailman/data /backup && \
-        cp -R /var/lib/mailman/lists /backup && \
-        cp -R /var/lib/mailman/archives /backup"
+        cp -Rp /var/lib/mailman/data /backup && \
+        cp -Rp /var/lib/mailman/lists /backup && \
+        cp -Rp /var/lib/mailman/archives /backup"
 
 # Clean up docker container:
 docker rm -f mailman_data_backup_cont
@@ -28,3 +33,6 @@ tar -cvf ./backup_${now}.tar $BACK_DIR
 
 # Remove the temporary backups folder:
 rm -R $BACK_DIR
+
+# Unset abort on error:
+set +e
--- restore_data.sh
+++ restore_data.sh
@@ -3,15 +3,13 @@
 # Restore backuped date into mailman.
 # Takes the backup file name (A tar file) as argument.
 
-# Abort if file doesn't exist:
+# Abort on failure.
 set -e
 
 BACK_DIR="backup_temp"
 
 # Extract the Tar file into backup_temp:
 tar -xvf $1 -C ./
-
-set +e
 
 # Backup the data, lists and archives mailman directories
 # by copying them to backup_temp directory on the host:
@@ -23,12 +21,15 @@ docker run --name mailman_data_restore_cont \
 	rm -rfv /var/lib/mailman/data/* && \
 	rm -rfv /var/lib/mailman/lists/* && \
 	rm -rfv /var/lib/mailman/archives/* && \
-        cp -R /backup/data /var/lib/mailman && \
-        cp -R /backup/lists /var/lib/mailman && \
-        cp -R /backup/archives /var/lib/mailman "
+        cp -pR /backup/data /var/lib/mailman && \
+        cp -pR /backup/lists /var/lib/mailman && \
+        cp -pR /backup/archives /var/lib/mailman "
 
 # Clean up: remove mailman_data_restore container:
 docker rm -f mailman_data_restore_cont
 
 # Remove the backups folder (We got it from opening the tar):
 rm -R $BACK_DIR
+
+# Unset abort on failure.
+set +e
