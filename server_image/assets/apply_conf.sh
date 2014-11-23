aa89337f36ed046b3a79424440c6be84852a131f
real <real.flayer@outlook.com>
2014-11-23 13:09:47 -0500

    Changed to server.conf, removed the EXPORT.

--- server_image/assets/apply_conf.sh
+++ server_image/assets/apply_conf.sh
@@ -5,7 +5,13 @@
 # See conf.sh for detailed information.
 
 # Get environment variables from conf.sh:
-source conf.sh
+# Using the -a setting we make sure that all variables
+# will be automatically exported. This is needed for the envsubset 
+# commands later.
+
+set -a
+source server.conf
+set +a
 
 # Replace environment variables with their value in some configuration files:
 envsubst < "etc-mailman-mm_cfg.py" | sponge "etc-mailman-mm_cfg.py"
