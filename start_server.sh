da736d468bc9b91fc74f709312219796aef9d9e2
real <real.flayer@outlook.com>
2014-11-23 11:41:45 -0500

    Checks if server is already up before starting.

--- start_server.sh
+++ start_server.sh
@@ -6,6 +6,14 @@
 # Abort on failure:
 set -e
 
+# Check if mailman_server_cont is running. 
+# If it is running, we abort.
+nlines_server=`docker ps | grep mailman_server_cont | wc -l`
+if [ "$nlines_server" -gt "0" ]
+	then echo "mailman_server_cont is already running! Aborting." && \
+		exit
+fi
+
 # Get the directories contents by running a new mailman_server:
 docker run -d --name  mailman_server_cont \
         -p 80:80 -p 25:25 \
