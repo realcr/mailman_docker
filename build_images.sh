3fce456c52c336ebe668f59258bed79125a19fab
real <real.flayer@outlook.com>
2014-11-23 14:31:42 -0500

    Added abort on failure.

--- build_images.sh
+++ build_images.sh
@@ -2,5 +2,11 @@
 
 # Build mailman_data and mailman_server images.
 
+# Abort on failure:
+set -e
+
 docker build -t mailman_server ./server_image
 docker build -t mailman_data ./data_image
+
+# Unset abort on failure:
+set +e
