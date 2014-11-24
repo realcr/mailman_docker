5fc2509926c2c68bbc2d8b79d2d0e34398cc5bd2
real <real.flayer@outlook.com>
2014-11-24 06:23:45 -0500

    Stop server will remove mailman_server_cont even if it doesn't run.

--- stop_server.sh
+++ stop_server.sh
@@ -6,13 +6,22 @@
 # Abort on failure:
 set -e
 
-# Check if mailman_server_cont is running. 
-# If it is not running, we abort.
-nlines_server=`docker ps | grep mailman_server_cont | wc -l`
-if [ "$nlines_server" -eq "0" ]
-	then echo "mailman_server_cont is not running! Aborting." && \
+# Check if the mailman_server_cont container exists:
+nlines_server_run=`docker ps -a | grep mailman_server_cont | wc -l`
+if [ "$nlines_server_run" -eq "0" ]
+	then echo "The mailman_server_cont container does not exist.
+Aborting." && \
 		exit
 fi
+
+# Check if mailman_server_cont is running. 
+# If it is not running, we abort.
+nlines_server_run=`docker ps | grep mailman_server_cont | wc -l`
+if [ "$nlines_server_run" -eq "0" ]
+	then echo "Note that mailman_server_cont is currently not running.
+Removing the container..."
+fi
+
 
 # Remove the mailman server container:
 docker rm -f mailman_server_cont
