78872a677154bb8fd7a18a1b5269fb617f9e6c11
real <real.flayer@outlook.com>
2014-11-26 11:47:21 -0500

    Moved to utils dir. Started writing backup script.

---
+++ utils/backup_script/backup_mailman.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Backup mailman data and send backup to google drive.
+# This script should be run periodically.
+
+
+# Full path of gdrive_docker dir:
+GDRIVE_DOCKER_PATH="/home/real/projects/gdrive_docker"
+# Full path of mailman_docker dir:
+MAILMAN_DOCKER_PATH="/home/real/projects/mailman_docker"
+
+
+
